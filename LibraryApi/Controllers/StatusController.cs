9c30ea44dfbbcf8db700345811e304f06fbb2b31
Dave Clifford <dclifford14@gmail.com>
2021-03-01 13:58:54 -0500

    initial commit

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("status")]
+        public StatusResponse GetTheStatus()
+        {
+            return new StatusResponse
+            {
+                Message = "Great Job!",
+                LastChecked = DateTime.Now
+            };
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+}
