8422a4ee24ba3b55bcfce31f30b8a7a2d5e1096a
Dave Clifford <dclifford14@gmail.com>
2021-03-02 16:41:04 -0500

    final day 2

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using LibraryApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +10,21 @@ namespace LibraryApi.Controllers
 {
     public class StatusController : ControllerBase
     {
+        private readonly ILookupServerStatus _statusLookup;
+
+        public StatusController(ILookupServerStatus statusLookup)
+        {
+            _statusLookup = statusLookup;
+        }
+
 
         // GET /status
         [HttpGet("status")]
         public StatusResponse GetTheStatus()
         {
-            return new StatusResponse
-            {
-                Message = "Everything is going great. Thanks for asking!",
-                LastChecked = DateTime.Now
-            };
+            // WTCYWYH
+            StatusResponse status = _statusLookup.GetStatusFor();
+            return status;
         }
 
         // GET /customers/13
